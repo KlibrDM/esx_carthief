8d57061b121cfda184fee67e46cf2f7fe41f4361
BackrndSource <back.rndsource@gmail.com>
2019-03-13 23:24:09 +0000

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,12 +1,12 @@
 Locales['en'] = {
-  ['vehicle_robbery'] = 'Vehicle Robbery!',
-  ['mission_failed'] = 'Mision Failed!',
-  ['get_back_car_10s'] = 'You have 10 seconds to get back in the car',
-  ['get_back_car_1m'] = 'You have 1 minute to get back in the car',
-  ['car_provided_rule'] = 'You have to use the car that was provided for you and you must come to a full stop.',
-  ['already_robbery'] = 'There is already a car robbery in progress!',
-  ['not_enough_cops'] = 'Not enough cops in town!',
-  ['car_stealing_in_progress'] = 'Car stealing in progress. Vehicle tracker will be active on your radar',
-  ['steal_a_car'] = 'Press ~INPUT_CONTEXT~ to steal a car',
-  ['drop_car_off'] = 'Press ~INPUT_CONTEXT~ to drop the car off'
+  ['vehicle_robbery'] = 'vehicle Robbery!',
+  ['mission_failed'] = 'mision Failed!',
+  ['get_back_car_10s'] = 'you have 10 seconds to get back in the car',
+  ['get_back_car_1m'] = 'you have 1 minute to get back in the car',
+  ['car_provided_rule'] = 'you have to use the car that was provided for you and you must come to a full stop.',
+  ['already_robbery'] = 'there is already a car robbery in progress!',
+  ['not_enough_cops'] = 'not enough cops in town!',
+  ['car_stealing_in_progress'] = 'car stealing in progress. Vehicle tracker will be active on your radar',
+  ['steal_a_car'] = 'press ~INPUT_CONTEXT~ to steal a car',
+  ['drop_car_off'] = 'press ~INPUT_CONTEXT~ to drop the car off'
 }
