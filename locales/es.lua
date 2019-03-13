20fc5b605a228c2c9b47eb950ba1dd8589ec0a65
BackrndSource <back.rndsource@gmail.com>
2019-03-13 23:22:16 +0000

    Create es.lua

---
+++ locales/es.lua
@@ -0,0 +1,12 @@
+Locales['es'] = {
+  ['vehicle_robbery'] = 'Robo de Vehículo!',
+  ['mission_failed'] = 'Misión Fallida!',
+  ['get_back_car_10s'] = 'Tienes 10 segundos para volver al vehículo',
+  ['get_back_car_1m'] = 'Tienes 1 minuto para volver al vehículo',
+  ['car_provided_rule'] = 'Debes de usar el vehículo que te fue proporcionado y detenerte por completo',
+  ['already_robbery'] = 'Ya hay un robo de vehículo en progreso!',
+  ['not_enough_cops'] = 'No hay suficiente policía en la ciudad!',
+  ['car_stealing_in_progress'] = 'Robo de vehículo en progreso. El seguimiento del vehículo estará disponible en tu radar',
+  ['steal_a_car'] = 'Pulsa ~INPUT_CONTEXT~ para robar un vehículo',
+  ['drop_car_off'] = 'Pulsa ~INPUT_CONTEXT~ para dejar el vehículo'
+}
