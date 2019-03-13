5fa72f3424237cf37305d56c9d0bd2625279099c
BackrndSource <back.rndsource@gmail.com>
2019-03-13 23:21:49 +0000

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,12 @@
+Locales['en'] = {
+  ['vehicle_robbery'] = 'Vehicle Robbery!',
+  ['mission_failed'] = 'Mision Failed!',
+  ['get_back_car_10s'] = 'You have 10 seconds to get back in the car',
+  ['get_back_car_1m'] = 'You have 1 minute to get back in the car',
+  ['car_provided_rule'] = 'You have to use the car that was provided for you and you must come to a full stop.',
+  ['already_robbery'] = 'There is already a car robbery in progress!',
+  ['not_enough_cops'] = 'Not enough cops in town!',
+  ['car_stealing_in_progress'] = 'Car stealing in progress. Vehicle tracker will be active on your radar',
+  ['steal_a_car'] = 'Press ~INPUT_CONTEXT~ to steal a car',
+  ['drop_car_off'] = 'Press ~INPUT_CONTEXT~ to drop the car off'
+}
